f197dd309d7ba12a569ea401326319c82c694c62
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-07-17 17:27:18 +0900

    P09再回答

--- P09/main.py
+++ P09/main.py
@@ -1,16 +1,13 @@
 def pack(Li):
     SL = []
     PL = []
-    st = 0
     for x in Li:
-        if x in SL or len(SL) == 0:
-            SL.append(x) 
+        if x in SL or len(SL) == 0:SL.append(x) 
         else:
             PL.append(SL)
             SL = []
-            SL.append(x) 
-        st+=1
-        if st == len(Li):
-            PL.append(SL) 
+            SL.append(x)
+    else:
+        if len(Li) > 0:PL.append(SL) 
     return PL
 
