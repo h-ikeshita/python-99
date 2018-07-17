742d3c9dbe414b163e0f09baf6774e0ebdd3d086
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-07-17 19:18:03 +0900

    P10再回答

--- P10/main.py
+++ P10/main.py
@@ -1,26 +1,18 @@
 def encode(DL):#DefaultList 
-    FXL = [] #FiXedList
-    SL = []  #SplitList  
-    MTlength = 1
-    for x in range(1,len(DL)):
-        if DL[x] == DL[x-1]:
-            MTlength+=1 
-            
-            if x == len(DL)-1:
-                SL.insert(0,MTlength)
-                SL.append(DL[x])
-                FXL.append(SL)
-                  
+    JL = []  #JudgeList
+    SL = []  #SplitList
+    AL = []  #AnswerList
+    for x in DL:
+        if x in JL or len(JL) == 0:JL.append(x)
         else:
-            SL.insert(0,MTlength)
-            SL.append(DL[x-1])
-            FXL.append(SL)
-            SL = []
-            MTlength = 1
-            
-            if x == len(DL)-1:
-                SL.insert(0,MTlength)
-                SL.append(DL[x])
-                FXL.append(SL)
+            SL = [len(JL),JL[0]]
+            AL.append(SL)
+            JL = [x]
+    else:
+        if len(JL) > 0:
+            SL = [len(JL),JL[0]]
+            AL.append(SL)
+    return AL
 
-    return FXL
+                  
+    
