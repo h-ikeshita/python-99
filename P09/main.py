8a0252c0860caffba88458e8cb871576d47fb7c2
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-07-19 15:34:40 +0900

    P09再回答

--- P09/main.py
+++ P09/main.py
@@ -1,13 +1,15 @@
-def pack(Li):
-    SL = []
-    PL = []
-    for x in Li:
-        if x in SL or len(SL) == 0:SL.append(x) 
+def pack(receive_list):
+    packed_list = []
+    target = 1
+    element_amount = 0
+    if len(receive_list)>0:
+        for source in receive_list:
+            if source != target:
+                packed_list.append([target]*element_amount)
+                element_amount = 0      
+                target = source
+            element_amount+=1
         else:
-            PL.append(SL)
-            SL = []
-            SL.append(x)
-    else:
-        if len(Li) > 0:PL.append(SL) 
-    return PL
+            packed_list.append([target]*element_amount)
+    return packed_list
 
