60f7e79b445d692f90a8c736aa113e88c16aeca2
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-07-23 13:59:51 +0900

    P11再回答

--- P11/main.py
+++ P11/main.py
@@ -1,19 +1,11 @@
-def encode_modified(DL): #DefaultList
-    AL = [] #AnswerList
-    JL = [] #JudgeList
-    for x in DL:
-        if x in JL or len(JL) == 0:JL.append(x)
+def encode_modified(receive_list):
+    encoded_list = []
+    for i in receive_list:
+        if encoded_list == [] or i != encoded_list[-1][-1]:
+            encoded_list.append([1,i])
         else:
-            if len(JL) == 1:
-                AL.append(JL[0])
-                JL = [x]
-            else:
-                SL = [len(JL),JL[0]]
-                AL.append(SL)
-                JL = [x]
-    else:
-       if len(JL) > 1:
-           SL = [len(JL),JL[0]]
-           AL.append(SL)
-       else:AL.append(JL[0])
-    return AL
+            encoded_list[-1][0] += 1
+    for i in encoded_list:
+        if i[0] == 1:
+            encoded_list[encoded_list.index(i)] = i[1]
+    return encoded_list                    
