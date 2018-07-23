98bd2a2a9d9e2b8219c51ffa68e7722932e66d6b
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-07-23 17:36:35 +0900

    P13途中経過

---
+++ P13/main.py
@@ -0,0 +1,9 @@
+def encode_direct(receive_list):
+    object_amount = 0
+    Progression_split = 0
+    for reference in receive_list:
+        if receive_list.index(reference) == 0 or reference == target:
+            object_amount+=1
+        else:
+            receive_list.append
+            del receive_list[Progress_split:target]
