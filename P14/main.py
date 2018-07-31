68b77104d4ed8ca000008af66d913563ecd9ee70
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-07-31 16:38:48 +0900

    P14回答

--- P14/main.py
+++ P14/main.py
@@ -1,7 +1,6 @@
 def duplicate(receive_list):
     dupled_list = []
-    dupled_list.extend(receive_list)
-    for origin in receive_list:
-            dupled_list.insert(dupled_list.index(origin),origin)
-    return dupled_list
+    for i in receive_list:
+        dupled_list.extend([i]*2)
+    return sorted(dupled_list)
 
