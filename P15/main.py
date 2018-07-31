14508c0a8a3ef25a5c4a2c74621c6bff5fb893f2
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-07-31 11:45:25 +0900

    P15再回答

--- P15/main.py
+++ P15/main.py
@@ -1,6 +1,5 @@
 def duplicate(receive_list,duple_times):
     dupled_list = []
-    for reference in receive_list:
-        for order in range(0,duple_times):
-            dupled_list.append(reference)
-    return dupled_list 
+    for i in range(duple_times):
+            dupled_list.extend(receive_list)
+    return sorted(dupled_list) 
