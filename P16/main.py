0a5c6fd7ae8cfe38dddd2522f874551580245165
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-08-01 14:49:59 +0900

    P16再回答

--- P16/main.py
+++ P16/main.py
@@ -1,9 +1,8 @@
 def drop(RL,Di):
     dropped_list = []
-    round = 1
-    for EL in RL:
-        if round % Di != 0:
-            dropped_list.append(EL)
-        round += 1
+    for i in RL:
+        if (RL.index(i)+1) % Di == 0:
+            continue
+        dropped_list.append(i)
     return dropped_list
     
