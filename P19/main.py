28bce05a6f3016842019b9768203e5138781f61b
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-08-01 16:40:55 +0900

    P19再回答

--- P19/main.py
+++ P19/main.py
@@ -1,10 +1,4 @@
-def rotate(receive_list,rotate_distance):
-    i = len(receive_list)
-    receive_list.extend(receive_list[:rotate_distance])
-    if rotate_distance > 0:    
-        del receive_list[:rotate_distance]
-    elif rotate_distance < 0:
-        del receive_list[:rotate_distance+i]
-    return receive_list
+def rotate(Receive,Split,Answer=[]):
+    return Receive[Split:] + Receive[:Split]
 
 
