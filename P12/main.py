960c8710c33c718a90c966317ae7c29dc3dc8f3b
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-08-06 10:12:46 +0900

    P12再回答

--- P12/main.py
+++ P12/main.py
@@ -2,7 +2,6 @@ def decode(DL):
     AL = []
     for x in DL:
         if type(x) == list:
-            for i in range(0,x[0]):
-                AL.append(x[1])
+               AL.extend([x[1]] * x[0])
         else:AL.append(x)
     return AL
