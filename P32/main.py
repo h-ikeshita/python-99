417cd43a2dadf196be816c5ec79a6c7ff96f7270
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-08-06 15:56:42 +0900

    P32再回答

--- P32/main.py
+++ P32/main.py
@@ -1,5 +1,4 @@
-def gcd(first,second):
-    for i in range(first,0,-1):
-        if first % i == 0:
-            if second % i == 0:
-                return i
+def gcd(origin,division):
+    while division != 0: 
+        origin,division = division,origin%division
+    return origin        
