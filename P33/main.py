9f3218fd2dc65a0e16a68b2a50a479560344137c
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-08-06 16:24:32 +0900

    P33

--- P33/main.py
+++ P33/main.py
@@ -1,6 +1,6 @@
+import gcd
+
 def is_coprime(first,second):
-    for i in range(first,1,-1):
-        if first % i == 0 and second % i == 0:
-            return False
-        else:continue
-    return True    
+    if gcd.gcd(first,second) == 1:
+        return True
+    return False    
