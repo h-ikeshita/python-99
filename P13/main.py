cf8a9648c6c464fab251fd6f0d668e4b270ad026
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-08-06 10:47:11 +0900

    P13再回答

--- P13/main.py
+++ P13/main.py
@@ -1,20 +1,17 @@
-def encode_direct(receive_list):
-    encoded_list = [] #回答用リスト
-    element_amount = 0　#(個数、要素）の個数側
-    target = 0　　　　　#要素側
-    for reference in receive_list:
-        if reference == target or element_amount == 0:
-            element_amount+=1
-        elif reference != target and element_amount == 1:
-            encoded_list.append(target)
-            target = reference
+def encode_direct(receive,encoded = []):
+    encoded.append(receive.pop(0))
+    for i in receive:
+        if type(encoded[-1]) == list:
+            if i == encoded[-1][-1]:
+                encoded[-1][0] += 1
+            else:
+                encoded.append(i)    
         else:
-            encoded_list.append([element_amount,target])
-            element_amount = 1
-        target = reference
-    else:#HACK:最後に取り残された要素の始末 XXX:最後の要素が単一だった場合は？ 
-        encoded_list.append([element_amount,target]) 
-    return encoded_list
+            if i == encoded[-1]:
+                encoded[-1] = [2,i]
+            else:
+                encoded.append(i)
+    return encoded
 
 
   
