29ef214daa1242f032f463f1a4043bc43a002c1a
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-08-08 01:20:28 +0900

    P34再回答

--- P34/main.py
+++ P34/main.py
@@ -1,11 +1,13 @@
+import sys
+sys.path.append('../')
+from P33.main import is_coprime
+from P32.main import gcd 
+
 def totient_phi(receive):
     answer = []
     for i in range(1,receive):
         p = receive - i
         if i >= p:break
-        for k in range(i,1,-1):
-            if i % k == 0 and p % k == 0:
-                break
-            else:continue
-        else:answer.extend([i,p])
-    return len(answer)   
+        if is_coprime(i,p) == True:
+            answer.extend([i,p])
+    return len(answer)
