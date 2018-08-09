f50d84f9cecf945e24bc0b6e0211b42e9f5fee5b
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-08-09 17:26:18 +0900

    P23再回答

--- P23/main.py
+++ P23/main.py
@@ -1,6 +1,4 @@
 import random
 def rnd_select(lotset,lottery):
-    lot_list = []
-    for i in range(lottery):
-        lot_list.append(random.choice(lotset))
-    return lot_list
+    return random.sample(lotset,k=lottery)
+
