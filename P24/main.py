e499d319f7d78773a0e388824fabbe5bcf767291
Ikeshitahikaru <Ikeshita@localhost.localdomain>
2018-08-10 13:44:01 +0900

    P24再回答

--- P24/main.py
+++ P24/main.py
@@ -1,7 +1,5 @@
 import random
 def rnd_select(lot_times,lotrange):
-    randomlist = []
-    for i in range(lotrange+1):
-        randomlist.append(i)
-    return random.sample(randomlist,k=lot_times)
+    list = range(lotrange+1)
+    return random.sample(list,k=lot_times)
     
